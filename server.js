51936bc3ac900e7007e25f8fd1a0e9cc78a6d709
Kafui Kpoh <qaphui@outlook.com>
2016-06-17 08:57:15 +0100

    Refactored API with underscore

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -21,12 +22,7 @@ app.get('/todos', function(req,res){
 //GET /todos/:id
 app.get('/todos/:id',function(req,res){
     var todoId = parseInt(req.params.id);
-    var matchedTodo;
-    todos.forEach(function(todo){
-        if (todo.id === todoId){
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos,{id: todoId});
     if(matchedTodo){
         res.json(matchedTodo);
     }else{
@@ -37,7 +33,12 @@ app.get('/todos/:id',function(req,res){
 
 //POST /todos/
 app.post('/todos',function(req,res){
-    var body = req.body;
+    var body = _.pick(req.body,'description','complete','id');
+    if(!_.isBoolean(body.complete) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
     //add id
     body.id = todoNextId;
     todoNextId++;
