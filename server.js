20f306eb07011ffe042a4a872b7c93bd23d46e95
Kafui Kpoh <qaphui@outlook.com>
2016-06-17 09:47:20 +0100

    Added DELETE todos route

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/todos', function(req,res){
 });
 //GET /todos/:id
 app.get('/todos/:id',function(req,res){
-    var todoId = parseInt(req.params.id);
+    var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos,{id: todoId});
     if(matchedTodo){
         res.json(matchedTodo);
@@ -45,6 +45,17 @@ app.post('/todos',function(req,res){
     //push todo
     todos.push(body);
     res.json(body);
+});
+
+app.delete('/todos/:id', function(req,res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos,{id:todoId});
+    if(!matchedTodo){
+        res.status(404).json({"error": "Request could not be completed!"})
+    }else{
+        todos = _.without(todos,matchedTodo);
+        res.json(matchedTodo);
+    }
 });
 
 app.listen(PORT, function(){
